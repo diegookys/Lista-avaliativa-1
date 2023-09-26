025b6618603ffbab1f9c0462efe99e8354bc8313
Diego Santos <133829130+diegookys@users.noreply.github.com>
2023-09-26 17:48:24 -0300

    Update Questão 5.c

--- Questão 5.c
+++ Questão 5.c
@@ -4,27 +4,25 @@ int main(){
     int A;
     int B;
 
-    printf("Digite o valor de A: ");
     scanf("%d", &A);
-    printf("Digite o valor de B: ");
     scanf("%d", &B);
 
     if(A == B){
         printf("Os valores lidos sao iguais\n");
     } else if (A > B){
         printf("%d eh maior que %d\n", A, B);
-        if(A % B == 0){
+    if(B != 0 && A % B == 0){
         printf("%d eh multiplo de %d\n", A, B);
-        } else {
+    } else {
         printf("%d nao eh multiplo de %d\n", A, B);
-        }
+    }
     } else {
         printf("%d eh maior que %d\n", B, A);
-        if(B % A == 0){
+    if(A != 0 && B % A == 0){
         printf("%d eh multiplo de %d\n", B, A);
-        } else {
+    } else {
         printf("%d nao eh multiplo de %d\n", B, A);
-        }
+    }
     }
 
     return 0;
